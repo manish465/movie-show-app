2608ede1f472ba657a8a23ceca076d7e0752c87e
manish465 <ms5978307@gmail.com>
2020-02-22 22:03:29 -0800

    added config.js to home for API fatching

--- src/componets/home/home.jsx
+++ src/componets/home/home.jsx
@@ -1,5 +1,15 @@
 import React, { Component } from "react";
+
 import "./home.css";
+
+import {
+    API_KEY,
+    IMAGE_BASE_URL,
+    BACKDROP_SIZE,
+    POSTER_SIZE,
+    API_URL
+} from "../../config";
+
 import Heroimage from "../element/heroimage/heroimage";
 import SeachBar from "../element/searchBar/searchBar";
 import FourColGrid from "../element/fourColGrid/fourColGrid";
@@ -8,7 +18,29 @@ import LoadMoreButton from "../element/loadMoreButton/loadMoreButton";
 import Spinner from "../element/spinner/spinner";
 
 class Home extends Component {
-    state = {};
+    state = {
+        movies: [],
+        heroimage: null,
+        isLoading: false,
+        currentPage: 0,
+        totalPages: 0,
+        searchTerms: ""
+    };
+
+    componentDidMount() {
+        this.setState({ isLoading: true });
+        const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-Us&page=1`;
+        this.fetchItems(endpoint);
+    }
+
+    fetchItems = endpoint => {
+        fetch(endpoint)
+            .then(result => result.json())
+            .then(result => {
+                console.log(result);
+            });
+    };
+
     render() {
         return (
             <div className="rmdb-home">
---
+++ src/config.js
@@ -0,0 +1,10 @@
+const API_URL = "https://api.themoviedb.org/3/";
+const API_KEY = "d58986401a9af5be776a0c136fb4326b";
+
+const IMAGE_BASE_URL = "https://image.tmbd.org.org/t/p/";
+
+const BACKDROP_SIZE = "w1280";
+
+const POSTER_SIZE = "w500";
+
+export { API_URL, API_KEY, IMAGE_BASE_URL, BACKDROP_SIZE, POSTER_SIZE };
